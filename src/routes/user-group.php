ab813b949b91c5eb16032ebf9f28e8905c7c8b42
Ronildo Souza <ronildo_rns@hotmail.com>
2017-10-05 12:27:19 -0300

    change url

--- src/routes/user-group.php
+++ src/routes/user-group.php
@@ -1,11 +1,11 @@
 <?php
 
-$app->get('/kpm/v1/user-groups', 'KPM\Controllers\UserGroupController:getAll');
+$app->get('/kpm/v1/users-group', 'KPM\Controllers\UserGroupController:getAll');
 
-$app->get('/kpm/v1/user-groups/{id}', 'KPM\Controllers\UserGroupController:getById');
+$app->get('/kpm/v1/users-group/{id}', 'KPM\Controllers\UserGroupController:getById');
 
-$app->post('/kpm/v1/user-groups', 'KPM\Controllers\UserGroupController:insertOrUpdate');
+$app->post('/kpm/v1/users-group', 'KPM\Controllers\UserGroupController:insertOrUpdate');
 
-$app->put('/kpm/v1/user-groups/{id}', 'KPM\Controllers\UserGroupController:insertOrUpdate');
+$app->put('/kpm/v1/users-group/{id}', 'KPM\Controllers\UserGroupController:insertOrUpdate');
 
-$app->delete('/kpm/v1/user-groups/{id}', 'KPM\Controllers\UserGroupController:delete');
+$app->delete('/kpm/v1/users-group/{id}', 'KPM\Controllers\UserGroupController:delete');
